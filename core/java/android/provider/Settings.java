c0d40b8106ee2a4b5836c276300c8fbc89123462
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    @neobuddy89: Clean up unused code and convert to 1-100 scale.
    Change-Id: I166326cfe6a19dc9b3d5f5c491d7e87e9c6a1913
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6181,6 +6181,11 @@ public final class Settings {
         public static final String BATTERY_FULL_LIGHT_ENABLED = "battery_full_light_enabled";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
