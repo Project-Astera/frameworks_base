8a65fbe96e5cc441b26bfd94edb54ef105d05734
tanxiaoyan <tanxiaoyan@xiaomi.com>
2023-01-12 12:38:53 +0800

    Avoid resursion for sleep check in embedding window
    Avoid recursion among check for sleep and complete pause during sleeping for embedding window.
    
    Bug:265231982
    Google: 2383980
    Change-Id: Iad507c29390f68559508e393a97770842160e35e
    Signed-off-by: tanxiaoyan <tanxiaoyan@xiaomi.com>

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -2013,7 +2013,8 @@ class TaskFragment extends WindowContainer<WindowContainer> {
     }
 
     boolean shouldSleepActivities() {
-        return false;
+        final Task task = getRootTask();
+        return task != null && task.shouldSleepActivities();
     }
 
     @Override
