d1b5296eee9ce124865e483cc539561d471cbce3
aswin7469 <aswinas@pixysos.com>
2023-03-23 15:02:10 +0530

    PixelPropUtils: Spoof userdebug to pixel launcher
    * so that we could enable developer options
    
    Change-Id: I527cbb84cec6e589bc41b87b9db01b726b736bfd

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -49,6 +49,7 @@ public class PixelPropsUtils {
     private static final boolean DEBUG = false;
 
     private static final Map<String, Object> propsToChangeGeneric;
+    private static final Map<String, Object> propsToChangeUserdebug;
     private static final Map<String, Object> propsToChangePixel7Pro;
     private static final Map<String, Object> propsToChangePixel5;
     private static final Map<String, Object> propsToChangePixelXL;
@@ -94,6 +95,10 @@ public class PixelPropsUtils {
             "com.google.android.apps.cameralite"
     };
 
+    private static final String[] packagesToChangeUserdebug = {
+            "com.google.android.apps.nexuslauncher"
+    };
+
     // Codenames for currently supported Pixels by Google
     private static final String[] pixelCodenames = {
             "felix",
@@ -118,6 +123,8 @@ public class PixelPropsUtils {
         propsToChangeGeneric = new HashMap<>();
         propsToChangeGeneric.put("TYPE", "user");
         propsToChangeGeneric.put("TAGS", "release-keys");
+        propsToChangeUserdebug = new HashMap<>();
+        propsToChangeUserdebug.put("TYPE", "userdebug");
         propsToChangePixel7Pro = new HashMap<>();
         propsToChangePixel7Pro.put("BRAND", "google");
         propsToChangePixel7Pro.put("MANUFACTURER", "Google");
@@ -223,6 +230,9 @@ public class PixelPropsUtils {
                     propsToChange.putAll(propsToChangePixel7Pro);
                 } else {
                     propsToChange.putAll(propsToChangePixel5);
+                }
+               if (Arrays.asList(packagesToChangeUserdebug).contains(packageName)) {
+                    propsToChange.putAll(propsToChangeUserdebug);
                 }
             }
 
