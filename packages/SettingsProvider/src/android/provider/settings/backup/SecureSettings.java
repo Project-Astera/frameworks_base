ba27fdd1909c7ca61b2be7b9e0f090dbbae5a5f5
aswin7469 <aswinas@pixysos.com>
2023-05-24 16:55:01 +0530

    base: Extend showing Ambient instead of fully waking to single tap gesture [1/2]
    Change-Id: I5e5bf835a1e8ecec59dbc1ee0563a6adf30ad6e9
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -227,5 +227,6 @@ public class SecureSettings {
         Settings.Secure.SHOW_WIFI_STANDARD_ICON,
         Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT,
     };
 }
