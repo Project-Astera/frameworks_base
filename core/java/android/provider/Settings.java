54ddb8ffa78399f843278e7cc47ca7baecdbe328
someone5678 <nemui3353@gmail.com>
2023-05-31 22:33:20 +0900

    base: Hide ADB and developer setting enable status [1/2]
    Squash of following commits:
    
    commit f9d292bfd32aa247265eba8091a25de21945ee05
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Sat Aug 12 17:01:41 2023 +0800
    
        base: HideDevStatus: move to getIntForUser to cover more scenarios
    
        * Adapt to current project as we want to set status individually per-app
          for case apps require ADB or Dev settings for their functionality
    
        e.g. SystemUI Tuner by Zachary Wander
        https://play.google.com/store/apps/details?id=com.zacharee1.systemuituner&hl=en_US&gl=US
    
        Ref:
        https://github.com/RisingTechOSS/android_frameworks_base/commit/77be7be42060dc5119c6dccef874c9a439aed210
    
        Change-Id: Ibbf92c7b1f1181d2508cb22ff2fc2017f9e59706
        Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    commit aa65314d2a032bfab4e0ad39412b154d44fd814b
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Sat Aug 12 00:33:29 2023 +0900
    
        fixup! base: Hide ADB and developer setting enable status [1/2]
    
        * Remove getCallingContext as we can simply use ContentResolver
    
        Change-Id: Ibc9d98ea2d1a878c9e93097f020db824da725913
    
    commit 40aa4d338016e2ffe2650cd83e313bf991133f93
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Wed May 31 22:33:20 2023 +0900
    
        base: Hide ADB and developer setting enable status [1/2]
    
        * Some apps check whether ADB or developer setting is enabled
        * Return 0 to selected apps when those apps getInt
          ADB_ENABLED, ADB_WIFI_ENABLED, DEVELOPMENT_SETTINGS_ENABLED
        * Code is adapted and stripped down from cutout force full screen
    
        Ref:
        https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
        https://github.com/yaap/frameworks_base/commit/232260f192effc9fb21a54eb444601179b24a26d
    
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -96,6 +96,8 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.util.Preconditions;
 import com.android.internal.widget.ILockSettings;
 
+import com.android.internal.util.custom.HideDeveloperStatusUtils;
+
 import java.io.IOException;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -3857,6 +3859,9 @@ public final class Settings {
         /** @hide */
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def, int userHandle) {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userHandle);
             return parseIntSettingWithDefault(v, def);
         }
@@ -3888,6 +3893,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int userHandle)
                 throws SettingNotFoundException {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userHandle);
             return parseIntSetting(v, name);
         }
@@ -7094,6 +7102,9 @@ public final class Settings {
         /** @hide */
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def, int userHandle) {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userHandle);
             return parseIntSettingWithDefault(v, def);
         }
@@ -7124,6 +7135,9 @@ public final class Settings {
         /** @hide */
         public static int getIntForUser(ContentResolver cr, String name, int userHandle)
                 throws SettingNotFoundException {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userHandle);
             return parseIntSetting(v, name);
         }
@@ -11702,6 +11716,13 @@ public final class Settings {
          * @hide
          */
         public static final String SYSTEM_BLACK_THEME = "system_black_theme";
+
+        /**
+         * Control whether to hide ADB and Developer settings enable status.
+         * @hide
+         */
+        @Readable
+        public static final String HIDE_DEVELOPER_STATUS = "hide_developer_status";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -17325,6 +17346,9 @@ public final class Settings {
          * or not a valid integer.
          */
         public static int getInt(ContentResolver cr, String name, int def) {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getString(cr, name);
             return parseIntSettingWithDefault(v, def);
         }
@@ -17349,6 +17373,9 @@ public final class Settings {
          */
         public static int getInt(ContentResolver cr, String name)
                 throws SettingNotFoundException {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getString(cr, name);
             return parseIntSetting(v, name);
         }
